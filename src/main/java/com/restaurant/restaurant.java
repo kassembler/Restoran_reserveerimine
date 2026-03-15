441c5aef076ebe7b5c0a25c88cbbdb0410fb911a
Kassembler <Kass@embler.ee>
2026-03-15 20:51:45 +0200

    Added sources for code templates.

--- src/main/java/com/restaurant/restaurant.java
+++ src/main/java/com/restaurant/restaurant.java
@@ -2,6 +2,7 @@ package com.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// template: https://spring.io/guides/gs/spring-boot
 
 @SpringBootApplication
 public class restaurant {
