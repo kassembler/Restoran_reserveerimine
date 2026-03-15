2616df9a063c76b7821444dfa2c1b8ec8c2a72a6
Kassembler <Kass@embler.ee>
2026-03-15 20:53:15 +0200

    Fixed table class parameters.

--- src/main/java/com/restaurant/model/Table.java
+++ src/main/java/com/restaurant/model/Table.java
@@ -4,10 +4,17 @@ public class Table {
 
     public int id;
     public int capacity;
-    public int x;
-    public int y;
 
     public boolean window;
     public boolean quiet;
+    public boolean outside;
+
+    public Table(int id, int capacity, boolean window, boolean quiet, boolean outside){
+    this.id = id;
+    this.capacity = capacity;
+    this.window = window;
+    this.quiet = quiet;
+    this.outside = outside;
+}
 
 }
